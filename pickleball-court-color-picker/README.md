194506cb8822974ca9938bef30b13f1402b5662f
Max Lythgoe <LYTHGOEMAX@GMAIL.COM>
2024-06-27 16:22:02 -0600

    Add files via upload

---
+++ pickleball-court-color-picker/README.md
@@ -0,0 +1,4 @@
+# Pickleball Court Color Picker
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/Max-Lythgoe-the-sans/pen/mdYgQyZ](https://codepen.io/Max-Lythgoe-the-sans/pen/mdYgQyZ).
+
---
+++ pickleball-court-color-picker/src/script.js
@@ -0,0 +1,28 @@
+document.querySelectorAll('.color-box').forEach(function(el) {
+    el.style.backgroundColor = el.getAttribute('data-color');
+});
+
+
+
+document.querySelector('#court-colors').addEventListener('click', function(event) {
+    if (event.target.matches('.color-box')) {
+        var color = event.target.getAttribute('data-color');
+        document.querySelector('#court').style.backgroundColor = color; 
+    }
+});
+
+document.querySelector('#border-colors').addEventListener('click', function(event) {
+    if (event.target.matches('.color-box')) {
+        var color = event.target.getAttribute('data-color');
+        document.querySelector('#border').style.backgroundColor = color; 
+    }
+});
+
+document.querySelector('#zone-colors').addEventListener('click', function(event) {
+    if (event.target.matches('.color-box')) {
+        var color = event.target.getAttribute('data-color');
+        document.querySelector('#net-zone').style.backgroundColor = color; 
+    }
+});
+
+  
